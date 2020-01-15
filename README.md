5282e65c787b569bbe52122435225d7de2663085
Christopher Smith <ctsmith2308@gmail.com>
2020-01-15 15:41:12 -0700

    quick socket.io setup

---
+++ README.md
@@ -0,0 +1,40 @@
+### Websocket Node Server with Socket.io
+
+Client side intergration with quick `create-react-app` startup
+
+```
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:8080"
+    };
+  }
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("hello", data => {
+      console.log(data);
+      this.setState({
+        response: data
+      });
+    });
+  }
+  render() {
+    const { response } = this.state;
+    return (
+      <div style={{ textAlign: "center" }}>
+        {response ? (
+          <p>The temperature in Florence is: {response} °F</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    );
+  }
+}
+export default App;
+```
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+
+const port = process.env.PORT || 8080;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let interval;
+io.on("connection", socket => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 3000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+const getApiAndEmit = socket => {
+  socket.emit("hello", "can you hear me?", 1, 2, "abc");
+  // try {
+  //   const res = await axios.get(
+  //     "https://api.darksky.net/forecast/b975dcca464db237ae1a6515a667ab62/37.8267,-122.4233"
+  //   ); // Getting the data from DarkSky
+  //   socket.emit("FromAPI", res.data.currently.temperature);
+  // } catch (error) {
+  //   console.log("here is the error", error);
+  //   console.error(`Error: ${error.code}`);
+  // }
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
